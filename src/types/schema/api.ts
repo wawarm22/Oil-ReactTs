6b98d826d58b23a64abbfc68cb4463d828018e8a
Mee(TB) <phongsak.ysd@gmail.com>
2025-04-04 20:01:40 +0700

    my-factory

--- src/types/schema/api.ts
+++ src/types/schema/api.ts
@@ -10,3 +10,20 @@ const ApiResponse = <T extends z.ZodTypeAny>(dataSchema: T) =>
 
 export type ApiLoginResponseSchema = z.infer<typeof ApiLoginResponseSchema>;
 export const ApiLoginResponseSchema = ApiResponse(AuthSchema);
+
+export type ApiMyFactorySchema = z.infer<typeof ApiMyFactorySchema>;
+export const ApiMyFactorySchema = ApiResponse(
+  z.array(
+    z.object({
+      user_id: z.number(),
+      factory_id: z.number(),
+      factory: z.object({
+        id: z.number(),
+        company_id: z.number(),
+        excise_id: z.string(),
+        name: z.string(),
+        slug: z.string(),
+      }),
+    })
+  )
+);
