83a8acc3aeab01fe300fa3c7f5b343fc969cf0a7
watcharaphon <watcharaphon.wa@gmail.com>
2025-04-28 12:52:39 +0700

    deploy failed

--- src/app/component/SubDocumentList.tsx
+++ src/app/component/SubDocumentList.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { documentList } from "../../types/docList";
-import MotionCard from "../reusable/MotionCard";
 
 interface SubDocumentListProps {
     selectedId: number | null;
@@ -9,19 +8,19 @@ interface SubDocumentListProps {
 
 const SubDocumentList: React.FC<SubDocumentListProps> = ({ selectedId, setSelectedDocIndex }) => {
     const selectedCategory = documentList.find(doc => doc.id === selectedId);
-    const [selectedSubIndex, setSelectedSubIndex] = useState<number | null>(null);
+    const [_selectedSubIndex, setSelectedSubIndex] = useState<number | null>(null);
 
     useEffect(() => {
-        if (selectedCategory && selectedCategory.documents!.length > 0) {
+        if (selectedCategory && selectedCategory.title!.length > 0) {
             setSelectedSubIndex(0);
             setSelectedDocIndex(0); 
         }
     }, [selectedId]);
 
-    const handleSubClick = (index: number) => {
-        setSelectedSubIndex(index);
-        setSelectedDocIndex(index); 
-    };
+    // const handleSubClick = (index: number) => {
+    //     setSelectedSubIndex(index);
+    //     setSelectedDocIndex(index); 
+    // };
 
     return (
         <div className="container-fluid d-flex justify-content-start align-items-start shadow-sm bg-white rounded-2 m-0 mt-3" style={{ padding: "25px 35px", maxWidth: "1020px" }}>
@@ -31,20 +30,7 @@ const SubDocumentList: React.FC<SubDocumentListProps> = ({ selectedId, setSelect
                 </p>
 
                 {selectedCategory && (
-                    <div className="d-flex flex-wrap justify-content-start">
-                        {selectedCategory.documents!.map((doc, index) => (
-                            <MotionCard
-                                key={index}
-                                isSelected={selectedSubIndex === index}
-                                onClick={() => handleSubClick(index)}
-                                width="240px"
-                                height="auto"
-                                textSize="16px"
-                                margin="m-2 py-2 px-3" 
-                            >
-                                {doc}
-                            </MotionCard>
-                        ))}
+                    <div className="d-flex flex-wrap justify-content-start">                            
                     </div>
                 )}
             </div>
